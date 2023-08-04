c8cfb84f5def69add25171f4fc9afcfe5cd52be6
karenjohn35 <90653104+karenjohn35@users.noreply.github.com>
2023-08-03 22:05:39 -0400

    post method working
    we just have to validate emails and phone numbers.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from 'express';
+
 import path from 'path';
 import {fileURLToPath} from 'url';
 import configRoutes from './routes/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+import router from './routes/register.js';
 
 const app = express();
+
+
+app.use(express.json());
+
+app.use("/", router);
 
 app.use(
   '/static',
